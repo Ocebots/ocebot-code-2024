b18bc9f75387f1a1d91847c9fc3ba4ce2653697c
BlueZeeKing <github@blueish.dev>
2024-01-08 12:38:08 -0500

    Add basic driving back in

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -17,9 +18,30 @@ public class RobotContainer {
 
   public RobotContainer() {
     configureBindings();
+    driveSubsystem.register();
+
+    driveSubsystem.setDefaultCommand(
+        Commands.run(
+            () -> {
+              driveSubsystem.drive(
+                  -MathUtil.applyDeadband(
+                      controller.getLeftY(), ControllerConstants.DRIVE_DEADBAND),
+                  -MathUtil.applyDeadband(
+                      controller.getLeftX(), ControllerConstants.DRIVE_DEADBAND),
+                  -MathUtil.applyDeadband(
+                      controller.getRightX(), ControllerConstants.DRIVE_DEADBAND),
+                  true,
+                  true);
+            },
+            driveSubsystem)); // Maybe change this?
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    this.controller.a().onTrue(Commands.runOnce(() -> this.driveSubsystem.zeroHeading(), this.driveSubsystem));
+    this.controller
+        .rightBumper()
+        .whileTrue(Commands.run(() -> this.driveSubsystem.setX(), this.driveSubsystem));
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
