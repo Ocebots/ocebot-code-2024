d9f9f00fae9a00c14b691b4b9e9c05c21b63e7b9
BlueZeeKing <github@blueish.dev>
2024-01-20 15:03:21 -0500

    Create basic subsystems

---
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class IntakeSubsystem {
+  public IntakeSubsystem() {}
+
+  private Command runIntake() {
+    return null; // TODO: Run the intake motors
+  }
+}
