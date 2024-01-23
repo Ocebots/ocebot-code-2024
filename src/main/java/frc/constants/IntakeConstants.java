97d0933e234d0970794e60a977a0c5dfe5c9e740
BlueZeeKing <github@blueish.dev>
2024-01-23 16:34:23 -0500

    Create `runIntake` method
    Closes #9

---
+++ src/main/java/frc/constants/IntakeConstants.java
@@ -0,0 +1,5 @@
+package frc.constants;
+
+public class IntakeConstants {
+  public static final double INTAKE_SPEED = 0.5;
+}
--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -3,15 +3,20 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import frc.constants.CANMappings;
+import frc.constants.IntakeConstants;
 
 public class IntakeSubsystem {
   private CANSparkMax motor = new CANSparkMax(CANMappings.INTAKE, MotorType.kBrushless);
 
   public IntakeSubsystem() {}
 
+  /**
+   * @return a command that will run forever that sets the motor to intake
+   */
   private Command runIntake() {
-    return null; // TODO: Run the intake motors, nothing else
+    return Commands.runEnd(() -> motor.set(IntakeConstants.INTAKE_SPEED), () -> motor.set(0));
   }
 
   public Command intake(ShootSubsystem shooter) {
