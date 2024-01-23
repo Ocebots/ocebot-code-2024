5be7e015fe100fe8623529bd97e13ec993d55158
Brayden Zee <github@blueish.dev>
2024-01-23 11:51:38 -0500

    Add intake motor

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,8 +1,13 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.constants.CANMappings;
 
 public class IntakeSubsystem {
+  private CANSparkMax motor = new CANSparkMax(CANMappings.INTAKE, MotorType.kBrushless);
+
   public IntakeSubsystem() {}
 
   private Command runIntake() {
