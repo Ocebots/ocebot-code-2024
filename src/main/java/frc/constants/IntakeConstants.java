a0d07e21525152ae5dffb086567b850f873a31e3
BlueZeeKing <github@blueish.dev>
2024-01-23 19:00:49 -0500

    Add intake motor config

--- src/main/java/frc/constants/IntakeConstants.java
+++ src/main/java/frc/constants/IntakeConstants.java
@@ -1,5 +1,9 @@
 package frc.constants;
 
+import com.revrobotics.CANSparkBase.IdleMode;
+
 public class IntakeConstants {
   public static final double INTAKE_SPEED = 0.5;
+  public static final IdleMode INTAKE_IDLE_MODE = IdleMode.kCoast;
+  public static final int CURRENT_LIMIT = 20;
 }
