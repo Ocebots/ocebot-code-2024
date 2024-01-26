26079273c726e5b1f7ea1d1b87364ab39d83f6ea
BlueZeeKing <github@blueish.dev>
2024-01-26 18:36:28 -0500

    Use controller for drivetrain

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,17 +4,17 @@
 
 package frc.robot;
 
-import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.constants.ControllerConstants;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.controller.Controller;
+import frc.robot.controller.XboxSoloController;
 import frc.robot.subsystems.DriveSubsystem;
 
 public class RobotContainer {
   DriveSubsystem driveSubsystem = new DriveSubsystem();
-  CommandXboxController controller =
-      new CommandXboxController(ControllerConstants.DRIVER_CONTROLLER_PORT);
+
+  Controller controller = new XboxSoloController();
 
   public RobotContainer() {
     configureBindings();
@@ -24,12 +24,9 @@ public class RobotContainer {
         Commands.run(
             () -> {
               driveSubsystem.drive(
-                  -MathUtil.applyDeadband(
-                      controller.getLeftY(), ControllerConstants.DRIVE_DEADBAND),
-                  -MathUtil.applyDeadband(
-                      controller.getLeftX(), ControllerConstants.DRIVE_DEADBAND),
-                  -MathUtil.applyDeadband(
-                      controller.getRightX(), ControllerConstants.DRIVE_DEADBAND),
+                  controller.getDriveX(),
+                  controller.getDriveY(),
+                  controller.getDriveTurn(),
                   true,
                   true);
             },
@@ -37,12 +34,6 @@ public class RobotContainer {
   }
 
   private void configureBindings() {
-    this.controller
-        .a()
-        .onTrue(Commands.runOnce(() -> this.driveSubsystem.zeroHeading(), this.driveSubsystem));
-    this.controller
-        .rightBumper()
-        .whileTrue(Commands.run(() -> this.driveSubsystem.setX(), this.driveSubsystem));
   }
 
   public Command getAutonomousCommand() {
--- src/main/java/frc/robot/controller/XboxSoloController.java
+++ src/main/java/frc/robot/controller/XboxSoloController.java
@@ -1,7 +1,9 @@
 package frc.robot.controller;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.constants.ControllerConstants;
 
 public class XboxSoloController implements Controller {
   CommandXboxController controller = new CommandXboxController(0);
@@ -33,16 +35,16 @@ public class XboxSoloController implements Controller {
 
   @Override
   public double getDriveX() {
-    return -controller.getLeftY();
+    return MathUtil.applyDeadband(-controller.getLeftY(), ControllerConstants.DRIVE_DEADBAND);
   }
 
   @Override
   public double getDriveY() {
-    return -controller.getLeftX();
+    return MathUtil.applyDeadband(-controller.getLeftX(), ControllerConstants.DRIVE_DEADBAND);
   }
 
   @Override
-  public double getDriveTurn() {
-    return controller.getRightX();
+  public double getDriveTurn() { // TODO: Do we need the negative?
+    return MathUtil.applyDeadband(-controller.getRightX(), ControllerConstants.DRIVE_DEADBAND);
   }
 }
