e9c1dbcf80b80881f19e6101526ab3b8b74f1fe2
BlueZeeKing <github@blueish.dev>
2024-01-28 15:49:06 -0500

    Fix height and tilt command scheduling issues

--- src/main/java/frc/robot/subsystems/ShootSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShootSubsystem.java
@@ -15,6 +15,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.constants.CANMappings;
@@ -153,7 +154,7 @@ public class ShootSubsystem extends SubsystemBase {
                       + elevatorFeedforward.calculate(desiredState.velocity));
             });
 
-    this.setDefaultCommand(tiltCommand.alongWith(heightCommand));
+    CommandScheduler.getInstance().schedule(tiltCommand, heightCommand);
   }
 
   private Command setVelocityOneSide(double velocity, CANSparkFlex motor, RelativeEncoder encoder) {
