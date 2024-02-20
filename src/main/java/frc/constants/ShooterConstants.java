8c8f63554d949621042f15119ac3eb77e272133e
BlueZeeKing <github@blueish.dev>
2024-02-20 10:54:55 -0500

    Update constants

--- src/main/java/frc/constants/ShooterConstants.java
+++ src/main/java/frc/constants/ShooterConstants.java
@@ -4,90 +4,90 @@ import com.revrobotics.CANSparkBase.IdleMode;
 import edu.wpi.first.math.geometry.Rotation2d;
 
 public class ShooterConstants {
-  public static double INTAKE_HEIGHT = 0;
-  public static Rotation2d INTAKE_ANGLE = Rotation2d.fromDegrees(0);
+  public static final double INTAKE_HEIGHT = 0;
+  public static final Rotation2d INTAKE_ANGLE = Rotation2d.fromDegrees(0);
 
-  public static class TiltConstants {
-    public static double GRAVITY_GAIN = 0;
-    public static double STATIC_GAIN = 0;
-    public static double VELOCITY_GAIN = 0;
+  public static final class TiltConstants {
+    public static final double GRAVITY_GAIN = 0;
+    public static final double STATIC_GAIN = 0;
+    public static final double VELOCITY_GAIN = 0;
 
-    public static double P_GAIN = 6;
-    public static double I_GAIN = 0;
-    public static double D_GAIN = 0;
+    public static final double P_GAIN = 6;
+    public static final double I_GAIN = 0;
+    public static final double D_GAIN = 0;
 
-    public static double MAX_ANGULAR_VELOCITY = 2.0;
-    public static double MAX_ANGULAR_ACCELERATION = 2.0;
+    public static final double MAX_ANGULAR_VELOCITY = 2.0;
+    public static final double MAX_ANGULAR_ACCELERATION = 2.0;
 
-    public static Rotation2d ANGLE_TOLERANCE = Rotation2d.fromDegrees(3);
+    public static final Rotation2d ANGLE_TOLERANCE = Rotation2d.fromDegrees(3);
 
-    public static double POSITION_CONVERSION_FACTOR = 2 * Math.PI;
-    public static double VELOCITY_CONVERSION_FACTOR = 2 * Math.PI / 60;
+    public static final double POSITION_CONVERSION_FACTOR = 2 * Math.PI;
+    public static final double VELOCITY_CONVERSION_FACTOR = 2 * Math.PI / 60;
 
-    public static IdleMode IDLE_MODE = IdleMode.kBrake;
-    public static int CURRENT_LIMIT = 50;
+    public static final IdleMode IDLE_MODE = IdleMode.kBrake;
+    public static final int CURRENT_LIMIT = 50;
   }
 
-  public static class HeightConstants {
-    public static double GRAVITY_GAIN = 0;
-    public static double STATIC_GAIN = 0;
-    public static double VELOCITY_GAIN = 0;
+  public static final class HeightConstants {
+    public static final double GRAVITY_GAIN = 0;
+    public static final double STATIC_GAIN = 0;
+    public static final double VELOCITY_GAIN = 0;
 
-    public static double P_GAIN = 200;
-    public static double I_GAIN = 0;
-    public static double D_GAIN = 0;
+    public static final double P_GAIN = 200;
+    public static final double I_GAIN = 0;
+    public static final double D_GAIN = 0;
 
-    public static double MAX_VELOCITY = 0.1;
-    public static double MAX_ACCELERATION = 0.5;
+    public static final double MAX_VELOCITY = 0.1;
+    public static final double MAX_ACCELERATION = 0.5;
 
-    public static double TOLERANCE = 0.03;
+    public static final double TOLERANCE = 0.03;
 
-    public static double POSITION_CONVERSION_FACTOR = 0.006;
-    public static double VELOCITY_CONVERSION_FACTOR = 0.0001;
+    public static final double POSITION_CONVERSION_FACTOR = 0.006;
+    public static final double VELOCITY_CONVERSION_FACTOR = 0.0001;
 
-    public static IdleMode IDLE_MODE = IdleMode.kBrake;
-    public static int CURRENT_LIMIT = 50;
+    public static final IdleMode IDLE_MODE = IdleMode.kBrake;
+    public static final int CURRENT_LIMIT = 50;
   }
 
-  public static class ShooterMotorConstants {
-    public static int CURRENT_LIMIT = 50;
-    public static IdleMode IDLE_MODE = IdleMode.kBrake;
+  public static final class ShooterMotorConstants {
+    public static final int CURRENT_LIMIT = 50;
+    public static final IdleMode IDLE_MODE = IdleMode.kBrake;
 
-    public static double TOLERANCE = 0.5;
+    public static final double TOLERANCE = 0.5;
 
-    public static double POSITION_CONVERSION_FACTOR = Math.PI * 0.0762;
-    public static double VELOCITY_CONVERSION_FACTOR = POSITION_CONVERSION_FACTOR / 60;
+    public static final double POSITION_CONVERSION_FACTOR = Math.PI * 0.0762;
+    public static final double VELOCITY_CONVERSION_FACTOR = POSITION_CONVERSION_FACTOR / 60;
 
-    public static double P_GAIN = 0;
-    public static double I_GAIN = 0;
-    public static double D_GAIN = 0;
+    public static final double P_GAIN = 0;
+    public static final double I_GAIN = 0;
+    public static final double D_GAIN = 0;
 
-    public static double STATIC_GAIN = 0;
-    public static double VELOCITY_GAIN = 0.6;
+    public static final double STATIC_GAIN = 0;
+    public static final double VELOCITY_GAIN = 0.6;
   }
 
-  public static class IntermediateConstants {
-    public static int CURRENT_LIMIT = 20;
-    public static IdleMode IDLE_MODE = IdleMode.kBrake;
+  public static final class IntermediateConstants {
+    public static final int CURRENT_LIMIT = 20;
+    public static final IdleMode IDLE_MODE = IdleMode.kCoast;
 
-    public static double SHOOT_SPEED = -1;
-    public static double SHOOT_TIME = 1;
+    public static final double SHOOT_SPEED = 1;
+    public static final double SHOOT_TIME = 1;
 
-    public static double P_GAIN = 0;
-    public static double I_GAIN = 0;
-    public static double D_GAIN = 0;
+    public static final double P_GAIN = 12;
+    public static final double I_GAIN = 0;
+    public static final double D_GAIN = 0;
 
-    public static double TOLERANCE = 0.5;
+    public static final double TOLERANCE = 0.02;
 
-    public static double POSITION_CONVERSION_FACTOR = 1 / 5 * Math.PI * 0.0508;
-    public static double VELOCITY_CONVERSION_FACTOR = POSITION_CONVERSION_FACTOR / 60;
+    public static final double POSITION_CONVERSION_FACTOR = (1.0 / 15.0) * Math.PI * 0.0508;
+    public static final double VELOCITY_CONVERSION_FACTOR = POSITION_CONVERSION_FACTOR / 60.0;
 
-    public static double STATIC_GAIN = 0;
-    public static double VELOCITY_GAIN = 0;
+    public static final double STATIC_GAIN = 0;
+    public static final double VELOCITY_GAIN = 0;
 
-    public static double MAX_VELOCITY = 0;
-    public static double MAX_ACCELERATION = 0;
+    public static final double MAX_VELOCITY = 1;
+    public static final double MAX_ACCELERATION = 1;
 
-    public static double FINAL_OFFSET = 0;
+    public static final double FINAL_OFFSET = 0.15;
   }
 }
