28506ed2af5c85e76b1d62e835b01b1ff97c7a65
Archonic <gabrielc2904@gmail.com>
2024-08-05 15:37:50 -0400

    Screw Formatting

--- src/main/java/frc/utils/APDS9960.java
+++ src/main/java/frc/utils/APDS9960.java
@@ -4,54 +4,53 @@ import edu.wpi.first.wpilibj.I2C;
 import frc.constants.VisionConstants;
 
 public class APDS9960 {
-    I2C i2c = new I2C(I2C.Port.kOnboard, VisionConstants.APDS9960_I2C_ADDRESS);
-    public APDS9960(){
-        init();
-    }
+  I2C i2c = new I2C(I2C.Port.kOnboard, VisionConstants.APDS9960_I2C_ADDRESS);
 
-    public void init(){
-        //enables power, sensor, interrupt
-        setAndPreserve((byte) 0x80, (byte) 0b00100101);
-    }
+  public APDS9960() {
+    init();
+  }
 
-    public void disableSensor(){
-        i2c.write(0x80, 0x00);
-    }
+  public void init() {
+    // enables power, sensor, interrupt
+    setAndPreserve((byte) 0x80, (byte) 0b00100101);
+  }
 
-    public void clearInterrupts(){
-        i2c.write((byte) 0xE5, (byte) 0);
-    }
+  public void disableSensor() {
+    i2c.write(0x80, 0x00);
+  }
 
-    public void setProximityPersistence(byte value){
-        value <<= 4;
-        setAndPreserve((byte) 0x8C, value);
-    }
+  public void clearInterrupts() {
+    i2c.write((byte) 0xE5, (byte) 0);
+  }
 
-    public void setProximityLowThreshold(byte thresh){
-        i2c.write((byte) 0x89, thresh);
-    }
+  public void setProximityPersistence(byte value) {
+    value <<= 4;
+    setAndPreserve((byte) 0x8C, value);
+  }
 
-    public void setProximityHighThreshold(byte thresh){
-        i2c.write((byte) 0x8B, thresh);
-    }
+  public void setProximityLowThreshold(byte thresh) {
+    i2c.write((byte) 0x89, thresh);
+  }
 
-    /**
-     *
-     * @param value If you'd like to set bit 2, use 0b01000000
-     */
-    private void setAndPreserve(byte reg, byte value){
-        byte[] read = new byte[1];
-        i2c.read(reg, 1, read);
-        i2c.write(reg, (byte) (read[0] | value));
-    }
+  public void setProximityHighThreshold(byte thresh) {
+    i2c.write((byte) 0x8B, thresh);
+  }
 
-    /**
-     *
-     * @param value If you'd like to unset bit 2, use 0b10111111
-     */
-    private void unsetAndPreserve(byte reg, byte value){
-        byte[] read = new byte[1];
-        i2c.read(reg, 1, read);
-        i2c.write(reg, (byte) (read[0] & value));
-    }
+  /**
+   * @param value If you'd like to set bit 2, use 0b01000000
+   */
+  private void setAndPreserve(byte reg, byte value) {
+    byte[] read = new byte[1];
+    i2c.read(reg, 1, read);
+    i2c.write(reg, (byte) (read[0] | value));
+  }
+
+  /**
+   * @param value If you'd like to unset bit 2, use 0b10111111
+   */
+  private void unsetAndPreserve(byte reg, byte value) {
+    byte[] read = new byte[1];
+    i2c.read(reg, 1, read);
+    i2c.write(reg, (byte) (read[0] & value));
+  }
 }
